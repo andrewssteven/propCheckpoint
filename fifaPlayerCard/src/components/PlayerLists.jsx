fb1b88042fcaad2ffdc8879eb6bfaa98dc3b87dc
andrewssteven <andrewssteven151@gmail.com>
2025-03-29 00:52:42 +0100

    modified

--- fifaPlayerCard/src/components/PlayerLists.jsx
+++ fifaPlayerCard/src/components/PlayerLists.jsx
@@ -5,7 +5,7 @@ import Player from './Player'
 
 const PlayerLists = () => {
   return (
-    <div className='grid grid-cols-4 gap-7 mx-15 mt-5'>
+    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-7 mx-15 mt-5'>
         {playerdata.map((player) => ( <Player name={player.name} team={player.team} 
         nationality={player.nationality} jerseynumber={player.jerseynumber} age={player.age}
         image={player.image}
